3211305c9ae91fb68f18b1bdf1c999085eadd537
juri931 <impa.mat@hotmail.it>
2024-03-19 10:30:10 +0100

    [add] send and receive message

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,5 @@
 const { createApp } = Vue;
+let DateTime = luxon.DateTime;
 
 import { contacts } from "./contacts.js";
 
@@ -6,10 +7,35 @@ createApp({
   data() {
     return {
       contacts,
-      activeContact: null,
+      activeContact: {},
+      newMessage: ''
     };
   },
-  methods: {},
+  methods: {
+    addNewMessage(){
+      const newMessage = this.generateMessage(this.newMessage, true);
+      this.activeContact.messages.push(newMessage);
+      this.newMessage = '';
+
+      const answerMsg = this.generateMessage('Ok!', false);
+      setTimeout(()=> this.activeContact.messages.push(answerMsg), 3000)
+    },
+
+    generateMessage(messageText, isSent){
+      const status= isSent ? 'sent' : 'received';
+      return{
+        // date: DateTime.now().selectLocale('it').toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS),
+        message: messageText,
+        status
+      }
+    },
+
+    doSearch(){
+      this.contacts.forEach(contact => {
+        contact.visible = contact.name.toUpperCase().includes(this.contactToSearch.toUpperCase())
+      })
+    }
+  },
   computed: {
     visibleContacts() {
       return this.contacts.filter((contact) => contact.visible);
