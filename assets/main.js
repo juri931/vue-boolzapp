ce07bd997f2167a8bf68a560249e1ea0c3323e2a
juri931 <impa.mat@gmail.com>
2024-09-25 13:01:18 +0200

    "Refactored main.js: added watch block, reformatted code, and moved doSearch method"

--- assets/main.js
+++ assets/main.js
@@ -8,33 +8,45 @@ createApp({
     return {
       contacts,
       activeContact: {},
-      newMessage: ''
+      newMessage: "",
     };
   },
+  watch: {
+      doSearch() {
+        this.contacts.forEach((contact) => {
+          contact.visible = contact.name
+            .toUpperCase()
+            .includes(this.contactToSearch.toUpperCase());
+        });
+      },
+    },
+    
   methods: {
-    addNewMessage(){
+    addNewMessage() {
       const newMessage = this.generateMessage(this.newMessage, true);
       this.activeContact.messages.push(newMessage);
-      this.newMessage = '';
+      this.newMessage = "";
 
-      const answerMsg = this.generateMessage('Ok!', false);
-      setTimeout(()=> this.activeContact.messages.push(answerMsg), 3000)
+      const answerMsg = this.generateMessage("Ok!", false);
+      setTimeout(() => this.activeContact.messages.push(answerMsg), 3000);
     },
 
-    generateMessage(messageText, isSent){
-      const status= isSent ? 'sent' : 'received';
-      return{
+    generateMessage(messageText, isSent) {
+      const status = isSent ? "sent" : "received";
+      return {
         // date: DateTime.now().selectLocale('it').toLocaleString(DateTime.DATETIME_SHORT_WITH_SECONDS),
         message: messageText,
-        status
-      }
+        status,
+      };
     },
 
-    doSearch(){
-      this.contacts.forEach(contact => {
-        contact.visible = contact.name.toUpperCase().includes(this.contactToSearch.toUpperCase())
-      })
-    }
+    doSearch() {
+      this.contacts.forEach((contact) => {
+        contact.visible = contact.name
+          .toUpperCase()
+          .includes(this.contactToSearch.toUpperCase());
+      });
+    },
   },
   computed: {
     visibleContacts() {
